eebc166d7be0cde4f01fbf981cc46e45f0f3ef92
Chloe D <owlgirlpnw@gmail.com>
2022-12-20 17:34:38 -0800

    Refactored character to characterright

--- characterright.js
+++ characterright.js
@@ -1,9 +1,8 @@
-class Character {
+class CharacterRight {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./character.png"), 30, 0, 380, 648, 4, 0.2);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./character_right.png"), 30, 0, 380, 648, 4, 0.2);
 
-        //this.x = 0;
         this.x = 1024 / 3;
         this.y = 0;
         this.speed = 100;
@@ -11,7 +10,6 @@ class Character {
 
     update() {
         this.x += this.speed*this.game.clockTick;
-        //if (this.x > 1024) this.x = -220;
         if (this.x > 1024 / 3) this.x = -220;
     };
 
--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./character.png");
+ASSET_MANAGER.queueDownload("./character_right.png");
 ASSET_MANAGER.queueDownload("./character_left.png");
 ASSET_MANAGER.queueDownload("./character_down.png");
 ASSET_MANAGER.queueDownload("./character_up.png");
-//ASSET_MANAGER.queueDownload("./snoopy_sprite_sheet.png")
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Character(gameEngine));
+	gameEngine.addEntity(new CharacterRight(gameEngine));
 	gameEngine.addEntity(new CharacterLeft(gameEngine));
 	gameEngine.addEntity(new CharacterDown(gameEngine));
 	gameEngine.addEntity(new CharacterUp(gameEngine));
