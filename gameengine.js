5a08d6260c1e4b363c6d53a224ab33d6703a46ab
Chloe D <owlgirlpnw@gmail.com>
2022-12-20 22:37:42 -0800

    Add line to canvas

--- gameengine.js
+++ gameengine.js
@@ -88,6 +88,21 @@ class GameEngine {
         for (let i = this.entities.length - 1; i >= 0; i--) {
             this.entities[i].draw(this.ctx, this);
         }
+
+        this.ctx.lineWidth = 5;
+        this.ctx.strokeStyle = 'red';
+        this.ctx.beginPath();
+
+        this.ctx.moveTo(1024 / 4,0);
+        this.ctx.lineTo(1024 / 4,230);
+
+        this.ctx.moveTo(1024 / 4,540);
+        this.ctx.lineTo(1024 / 4,768);
+
+        this.ctx.moveTo(440,0);
+        this.ctx.lineTo(440,768);
+
+        this.ctx.stroke();
     };
 
     update() {
@@ -114,6 +129,6 @@ class GameEngine {
         this.draw();
     };
 
-};
+}
 
 // KV Le was here :)
